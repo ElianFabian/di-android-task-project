16cd1c63c288390bc151c007e63230ff0c014d42
Elián <elian.fabian.el.belaizi@gmail.com>
2022-03-22 17:42:54 +0100

    Spinner importance from TaskAdd is complete.
    An array of the importance types has been added and the spinner adapter is initialized on onStart().

--- app/src/main/java/com/elian/myapplication/ui/taskadd/TaskAddFragment.kt
+++ app/src/main/java/com/elian/myapplication/ui/taskadd/TaskAddFragment.kt
@@ -1,22 +1,43 @@
 package com.elian.myapplication.ui.taskadd
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import com.elian.myapplication.R
 import com.elian.myapplication.databinding.FragmentTaskAddBinding
 
 class TaskAddFragment : Fragment()
 {
     private lateinit var binding: FragmentTaskAddBinding
-    
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?,
     ): View
     {
         binding = FragmentTaskAddBinding.inflate(inflater)
         return binding.root
+    }
+
+    override fun onStart()
+    {
+        super.onStart()
+
+        initSpinnerAdapter()
+    }
+
+    // This function should be called on onStart()
+    private fun initSpinnerAdapter()
+    {
+        val spinnerAdapter = ArrayAdapter(
+            activity as Context,
+            android.R.layout.simple_spinner_dropdown_item,
+            resources.getStringArray(R.array.importance)
+        )
+    
+        binding.spImportance.adapter = spinnerAdapter
     }
 }
