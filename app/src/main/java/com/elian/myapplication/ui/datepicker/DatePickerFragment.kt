5baaa2e1cc63dfffcc683a32503499fa7fe3e1c7
Elián <elian.fabian.el.belaizi@gmail.com>
2022-03-23 17:09:14 +0100

    Update DatePickerFragment.kt

--- app/src/main/java/com/elian/myapplication/ui/datepicker/DatePickerFragment.kt
+++ app/src/main/java/com/elian/myapplication/ui/datepicker/DatePickerFragment.kt
@@ -1,15 +1,35 @@
 package com.elian.myapplication.ui.datepicker
 
 import android.app.DatePickerDialog
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
 import android.widget.DatePicker
 import androidx.fragment.app.DialogFragment
+import java.util.*
 
-class DatePickerFragment(val listener: (day: Int, month: Int, year: Int) -> Unit) :
+class DatePickerFragment(val listener: (year: Int, month: Int, dayOfMonth: Int) -> Unit) :
 
     DialogFragment(), DatePickerDialog.OnDateSetListener
 {
+    /**
+     * This method it's called when the user picks a date.
+     */
     override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int)
     {
+        listener(year, month, dayOfMonth)
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog
+    {
+        val calendar = Calendar.getInstance()
         
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+        val month = calendar.get(Calendar.MONTH)
+        val year = calendar.get(Calendar.YEAR)
+        
+        val picker = DatePickerDialog(activity as Context, this, year, month, day)
+        
+        return picker
     }
 }
