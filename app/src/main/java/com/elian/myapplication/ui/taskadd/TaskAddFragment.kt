4896c40a112aa781c254d8e7f42ded2612ed120e
Elián <elian.fabian.el.belaizi@gmail.com>
2022-03-23 18:45:08 +0100

    Update TaskAddFragment.kt

--- app/src/main/java/com/elian/myapplication/ui/taskadd/TaskAddFragment.kt
+++ app/src/main/java/com/elian/myapplication/ui/taskadd/TaskAddFragment.kt
@@ -1,15 +1,11 @@
 package com.elian.myapplication.ui.taskadd
 
-import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.Toast
 import androidx.navigation.fragment.NavHostFragment
-import com.elian.myapplication.R
 import com.elian.myapplication.databinding.FragmentTaskAddBinding
 import com.elian.myapplication.ui.datepicker.DatePickerFragment
 
