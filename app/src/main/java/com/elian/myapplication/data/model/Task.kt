a38670adc805ec749135af96bb07f25cc6dfe120
Elián <elian.fabian.el.belaizi@gmail.com>
2022-03-24 09:59:08 +0100

    Update Task.kt

--- app/src/main/java/com/elian/myapplication/data/model/Task.kt
+++ app/src/main/java/com/elian/myapplication/data/model/Task.kt
@@ -1,6 +1,6 @@
 package com.elian.myapplication.data.model
 
-data class Task(val name: String, val importance: Importance)
+data class Task(val name: String, val importance: Importance, val endDate: Long)
 {
     enum class Importance
     {
