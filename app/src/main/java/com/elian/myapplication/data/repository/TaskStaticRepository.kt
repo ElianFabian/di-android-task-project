31785cf7138e9255d8d5518b8fac8fcbe125ce11
Elián <elian.fabian.el.belaizi@gmail.com>
2022-03-30 09:34:47 +0200

    Update TaskStaticRepository.kt

--- app/src/main/java/com/elian/myapplication/data/repository/TaskStaticRepository.kt
+++ app/src/main/java/com/elian/myapplication/data/repository/TaskStaticRepository.kt
@@ -1,12 +1,15 @@
 package com.elian.myapplication.data.repository
 
 import com.elian.myapplication.data.model.Task
+import java.util.*
 
 class TaskStaticRepository
 {
     companion object
     {
         val instance = TaskStaticRepository()
+        
+        private val calendar = Calendar.getInstance()
     }
 
     private val taskList = arrayListOf(
@@ -14,25 +17,25 @@ class TaskStaticRepository
             name = "tarea1",
             description = "es una tarea",
             importance = Task.Importance.values().random(),
-            endDate = 12345678
+            endDate = getRandomDate()
         ),
         Task(
             name = "tarea2",
             description = "es una tarea",
             importance = Task.Importance.values().random(),
-            endDate = 12345678
+            endDate = getRandomDate()
         ),
         Task(
             name = "tarea3",
             description = "es una tarea",
             importance = Task.Importance.values().random(),
-            endDate = 12345678
+            endDate = getRandomDate()
         ),
         Task(
             name = "tarea4",
             description = "es una tarea",
             importance = Task.Importance.values().random(),
-            endDate = 12345678
+            endDate = getRandomDate()
         )
     )
 
@@ -44,6 +47,11 @@ class TaskStaticRepository
     }
 
     fun getList() = taskList
+
+    /**
+     * Returns a date from now until 10 years ago.
+     */
+    private fun getRandomDate(): Long = calendar.timeInMillis - (0.. 31_556_900_000 * 10).random()
 
     //endregion
 }
