c9d7b829d6ff31841133fd1acd6a1920b4e4f609
Elián <elian.fabian.el.belaizi@gmail.com>
2022-03-30 10:55:26 +0200

    Update ITaskAddContract.kt

--- app/src/main/java/com/elian/myapplication/ui/taskadd/ITaskAddContract.kt
+++ app/src/main/java/com/elian/myapplication/ui/taskadd/ITaskAddContract.kt
@@ -2,4 +2,23 @@ package com.elian.myapplication.ui.taskadd
 
 interface ITaskAddContract
 {
+    interface IView
+    {
+        
+    }
+    
+    interface IPresenter
+    {
+        
+    }
+    
+    interface IInteractor
+    {
+        
+    }
+    
+    interface IRepository
+    {
+        
+    }
 }
