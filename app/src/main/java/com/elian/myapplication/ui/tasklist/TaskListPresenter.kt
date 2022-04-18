737bc995917a9ed91d10b7ccb8995a270bba89aa
Elián <elian.fabian.el.belaizi@gmail.com>
2022-04-18 12:38:07 +0200

    Update TaskListPresenter.kt
    IRepositoryListCallback implemented.

--- app/src/main/java/com/elian/myapplication/ui/tasklist/TaskListPresenter.kt
+++ app/src/main/java/com/elian/myapplication/ui/tasklist/TaskListPresenter.kt
@@ -1,10 +1,27 @@
 package com.elian.myapplication.ui.tasklist
 
-class TaskListPresenter: ITaskListContract.IPresenter
+import com.elian.myapplication.base.IRepositoryListCallback
+import com.elian.myapplication.data.model.Task
+
+class TaskListPresenter: ITaskListContract.IPresenter, IRepositoryListCallback
 {
     //region ITaskListContract.IPresenter
 
     override fun load()
+    {
+        TODO("Not yet implemented")
+    }
+
+    //endregion
+    
+    //region IRepositoryListCallback
+
+    override fun onSuccess(list: List<Task>)
+    {
+        TODO("Not yet implemented")
+    }
+
+    override fun onNoData()
     {
         TODO("Not yet implemented")
     }
