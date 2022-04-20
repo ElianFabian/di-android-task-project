3eab5078d6afbdf33ed1a0b0d31138c08847a303
Elián <elian.fabian.el.belaizi@gmail.com>
2022-04-20 09:18:49 +0200

    Update TaskListPresenter.kt

--- app/src/main/java/com/elian/myapplication/ui/tasklist/TaskListPresenter.kt
+++ app/src/main/java/com/elian/myapplication/ui/tasklist/TaskListPresenter.kt
@@ -1,6 +1,5 @@
 package com.elian.myapplication.ui.tasklist
 
-import com.elian.myapplication.base.IRepositoryListCallback
 import com.elian.myapplication.data.model.Task
 
 class TaskListPresenter(private var view: ITaskListContract.IView):
