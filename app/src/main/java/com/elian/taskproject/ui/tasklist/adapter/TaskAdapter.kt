4cf960e0f083dfb970ade697cd5de02db1915e2f
Elián <elian.fabian.el.belaizi@gmail.com>
2022-04-21 17:40:50 +0200

    Update TaskAdapter.kt

--- app/src/main/java/com/elian/taskproject/ui/tasklist/adapter/TaskAdapter.kt
+++ app/src/main/java/com/elian/taskproject/ui/tasklist/adapter/TaskAdapter.kt
@@ -12,7 +12,8 @@ import java.util.*
 import kotlin.collections.ArrayList
 
 class TaskAdapter(
-    private val tasks: ArrayList<Task>, private val listener: View.OnClickListener,
+    private val tasks: ArrayList<Task>,
+    private val listener: View.OnClickListener,
 ) :
     RecyclerView.Adapter<TaskAdapter.ViewHolder>()
 {
