8274ab61f82d11b18ce9384d98433d47aaa3dc12
Elián <elian.fabian.el.belaizi@gmail.com>
2022-04-21 11:33:07 +0200

    Update BaseFragment.kt

--- app/src/main/java/com/elian/taskproject/base/BaseFragment.kt
+++ app/src/main/java/com/elian/taskproject/base/BaseFragment.kt
@@ -5,11 +5,11 @@ import androidx.fragment.app.Fragment
 abstract class BaseFragment : Fragment()
 {
     internal abstract val presenter: IBasePresenter
-    
+
     override fun onDestroy()
     {
         super.onDestroy()
-        
+
         presenter.onDestroy()
     }
 }
