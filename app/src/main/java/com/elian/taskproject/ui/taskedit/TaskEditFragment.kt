5a450bdc48f6d46b9db3ac5e8ec71ede3148aa20
Elián <elian.fabian.el.belaizi@gmail.com>
2022-04-26 22:44:53 +0200

    Created TaskEditFragment

---
+++ app/src/main/java/com/elian/taskproject/ui/taskedit/TaskEditFragment.kt
@@ -0,0 +1,23 @@
+package com.elian.taskproject.ui.taskedit
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.elian.taskproject.R
+import com.elian.taskproject.databinding.FragmentTaskEditBinding
+
+
+class TaskEditFragment : Fragment()
+{
+    private lateinit var binding: FragmentTaskEditBinding
+    
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View
+    {
+        binding = FragmentTaskEditBinding.inflate(inflater)
+        return binding.root
+    }
+}
