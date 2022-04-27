de361d3a75df0f5e8189a4e58a4623e128a70592
Elián <elian.fabian.el.belaizi@gmail.com>
2022-04-27 10:15:43 +0200

    Update TaskEditFragment.kt
    Implemented fillFieldsWithSelectedTask() function

--- app/src/main/java/com/elian/taskproject/ui/taskedit/TaskEditFragment.kt
+++ app/src/main/java/com/elian/taskproject/ui/taskedit/TaskEditFragment.kt
@@ -6,11 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.elian.taskproject.R
+import com.elian.taskproject.data.model.Task
+import com.elian.taskproject.data.utils.DataUtils
 import com.elian.taskproject.databinding.FragmentTaskEditBinding
+import com.elian.taskproject.ui.taskadd.TaskAddFragment
+import java.time.LocalDate
+import java.util.*
 
 
 class TaskEditFragment : Fragment()
 {
+    //region Fragment Methods
+
     private lateinit var binding: FragmentTaskEditBinding
 
     override fun onCreateView(
@@ -20,4 +27,18 @@ class TaskEditFragment : Fragment()
         binding = FragmentTaskEditBinding.inflate(inflater)
         return binding.root
     }
+
+    //endregion
+
+    //region Methods
+
+    private fun fillFieldsWithSelectedTask(task: Task) = with(binding)
+    {
+        tieName.setText(task.name)
+        tieDescription.setText(task.description)
+        spImportance.setSelection(task.importance.ordinal)
+        etDate.setText(DataUtils.dateFormat.format(Date(task.endDate as Long)))
+    }
+
+    //endregion
 }
