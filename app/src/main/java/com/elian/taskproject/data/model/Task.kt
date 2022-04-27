41f699e0b5bf2bc8ec107b84216bb3279cb98dc5
Elián <elian.fabian.el.belaizi@gmail.com>
2022-04-27 09:42:51 +0200

    Update Task.kt
    Implemented Serializable

--- app/src/main/java/com/elian/taskproject/data/model/Task.kt
+++ app/src/main/java/com/elian/taskproject/data/model/Task.kt
@@ -1,12 +1,15 @@
 package com.elian.taskproject.data.model
 
+import java.io.Serializable
+
 data class Task(
     var name: String,
     var description: String,
     var importance: Importance,
     var endDateEstimated: Long?,
     var endDate: Long? = null,
-)
+) :
+    Serializable
 {
     enum class Importance
     {
