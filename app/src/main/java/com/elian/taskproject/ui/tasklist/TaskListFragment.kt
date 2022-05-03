68036493289e55320f76c34c9bf0757ae1371d99
Elián <elian.fabian.el.belaizi@gmail.com>
2022-05-03 19:09:33 +0200

    Update TaskListFragment.kt

--- app/src/main/java/com/elian/taskproject/ui/tasklist/TaskListFragment.kt
+++ app/src/main/java/com/elian/taskproject/ui/tasklist/TaskListFragment.kt
@@ -139,6 +139,14 @@ class TaskListFragment : BaseFragment(),
             {
                 swIsEndDate.isChecked = it < Calendar.getInstance().timeInMillis
             }
+
+//            swIsEndDate.setOnClickListener()
+//            {
+//                val timeInMillis = Calendar.getInstance().timeInMillis
+//
+//                if (swIsEndDate.isChecked || item.estimatedEndDate!! > timeInMillis) item.endDate = timeInMillis
+//                else item.endDate = null
+//            }
         }
     }
 
