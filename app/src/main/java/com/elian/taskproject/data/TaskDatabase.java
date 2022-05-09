1f546b386aa6ab363f72b35af165adbf8cd61d3e
Elián <elian.fabian.el.belaizi@gmail.com>
2022-05-09 11:00:35 +0200

    Added Room

---
+++ app/src/main/java/com/elian/taskproject/data/TaskDatabase.java
@@ -0,0 +1,76 @@
+package com.elian.taskproject.data;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+// 1. Define the configuration of the database.
+
+public abstract class TaskDatabase extends RoomDatabase {
+
+    // 2. Create the methods to get the DAO.
+
+    private static volatile TaskDatabase INSTANCE;
+    private static final int NUMBER_OF_THREADS = 4;
+    public static final ExecutorService databaseWriteExecutor =
+            Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+
+    static TaskDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (TaskDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            TaskDatabase.class, "task")
+                            .fallbackToDestructiveMigration()
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+
+    public static void create(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (TaskDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            TaskDatabase.class, "inventory")
+                            .addCallback(new Callback() {
+                                @Override
+                                public void onCreate(@NonNull SupportSQLiteDatabase db) {
+                                    super.onCreate(db);
+                                    //databaseWriteExecutor.execute(() -> prepopulate(context));
+                                }
+                            })
+                            .fallbackToDestructiveMigration()
+                            .build();
+                }
+            }
+        }
+    }
+
+    public static TaskDatabase getDatabase() {
+        return INSTANCE;
+    }
+
+    /**
+     * Function when on create insert the data we need.
+     */
+//    private static void prepopulate(Context context) {
+//
+//        UserDao userDao = INSTANCE.userDao();
+//        User user = new User("moronlu18@gmail.com", "Lourdes18?");
+//        getDatabase().runInTransaction(() -> {
+//            userDao.insert(user);
+//        });
+//    }
+}
