abfa533358a26ad93d6acb581abf1bf6299f5a67
Elián <elian.fabian.el.belaizi@gmail.com>
2022-05-09 11:14:41 +0200

    Created TaskApplication and added into AndroidManifest.xml

---
+++ app/src/main/java/com/elian/taskproject/TaskApplication.kt
@@ -0,0 +1,20 @@
+package com.elian.taskproject
+
+import android.app.Application
+import com.elian.taskproject.data.TaskDatabase
+
+// This class must be added into the AndroidManifest.xml in order to work
+/*
+<application
+            android:name=".TaskApplication"
+            ...
+ */
+class TaskApplication : Application()
+{
+    override fun onCreate()
+    {
+        super.onCreate()
+        
+        TaskDatabase.create(this)
+    }
+}
