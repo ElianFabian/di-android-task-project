a2357da0c793f3a43cd372e6c1c86a65fbd5626d
Elián <elian.fabian.el.belaizi@gmail.com>
2022-05-09 12:35:38 +0200

    Create TaskRepository.kt

---
+++ app/src/main/java/com/elian/taskproject/data/repository/TaskRepository.kt
@@ -0,0 +1,50 @@
+package com.elian.taskproject.data.repository
+
+import com.elian.taskproject.data.TaskDatabase
+import com.elian.taskproject.data.model.Task
+import com.elian.taskproject.ui.taskmanager.ITaskManagerContract
+import com.elian.taskproject.ui.tasklist.ITaskListContract
+
+object TaskRepository :
+    ITaskListContract.IRepository,
+    ITaskManagerContract.IRepository
+{
+    //region ITaskListContract.IRepository
+
+    override fun getList(callback: ITaskListContract.IOnRepositoryCallback)
+    {
+        if (TaskDatabase.getDatabase().taskDAO.selectAll().isEmpty())
+        {
+            callback.onNoData()
+        }
+        else callback.onListSuccess(TaskDatabase.getDatabase().taskDAO.selectAll())
+    }
+
+    override fun delete(callback: ITaskListContract.IOnRepositoryCallback, task: Task)
+    {
+        val taskList = TaskDatabase.getDatabase().taskDAO.selectAll()
+
+        val position = taskList.indexOf(task)
+        
+        TaskDatabase.getDatabase().taskDAO.delete(task)
+        callback.onDeleteSuccess(task, position)
+    }
+
+    //endregion
+
+    //region ITaskAddContract.IRepository
+
+    override fun add(callback: ITaskManagerContract.IOnRepositoryCallback, task: Task)
+    {
+        TaskDatabase.getDatabase().taskDAO.insert(task)
+        callback.onAddSuccess()
+    }
+
+    override fun edit(callback: ITaskManagerContract.IOnRepositoryCallback, editedTask: Task, position: Int)
+    {
+        TaskDatabase.getDatabase().taskDAO.update(editedTask)
+        callback.onEditSuccess()
+    }
+
+    //endregion
+}
