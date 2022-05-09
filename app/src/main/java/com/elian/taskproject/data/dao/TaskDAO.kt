45e5b12eaca309c683fdbd42a410ab532e02a26f
Elián <elian.fabian.el.belaizi@gmail.com>
2022-05-09 12:08:52 +0200

    Update TaskDAO.kt

--- app/src/main/java/com/elian/taskproject/data/dao/TaskDAO.kt
+++ app/src/main/java/com/elian/taskproject/data/dao/TaskDAO.kt
@@ -1,14 +1,25 @@
 package com.elian.taskproject.data.dao
 
+import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Query
 import androidx.room.Update
 import com.elian.taskproject.data.model.Task
 
 interface TaskDAO
 {
+    @Query("SELECT * FROM task_table WHERE id = :id")
+    fun select(id: Long) : Task
+
     @Insert
-    fun insert(task: Task)
-    
+    fun insert(task: Task): Long
+
     @Update
     fun update(task: Task)
+
+    @Delete
+    fun delete(task: Task)
+
+    @Query("DELETE FROM task_table")
+    fun deleteAll()
 }
