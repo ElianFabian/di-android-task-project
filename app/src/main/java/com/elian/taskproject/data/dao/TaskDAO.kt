7c9d6b457753dc84f4b548055105c1b690f990ac
Elián <elian.fabian.el.belaizi@gmail.com>
2022-05-09 11:11:15 +0200

    Update TaskDAO.kt

--- app/src/main/java/com/elian/taskproject/data/dao/TaskDAO.kt
+++ app/src/main/java/com/elian/taskproject/data/dao/TaskDAO.kt
@@ -1,5 +1,10 @@
 package com.elian.taskproject.data.dao
 
+import androidx.room.Insert
+import com.elian.taskproject.data.model.Task
+
 interface TaskDAO
 {
+    @Insert
+    fun insert(task: Task)
 }
