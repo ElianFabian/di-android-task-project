4043527bb323c6e5029caf62fa9a4d161bb47a61
Elián <elian.fabian.el.belaizi@gmail.com>
2022-05-12 14:24:19 +0200

    Create UserDAO.kt

---
+++ app/src/main/java/com/elian/taskproject/data/dao/UserDAO.kt
@@ -0,0 +1,14 @@
+package com.elian.taskproject.data.dao
+
+import androidx.room.*
+import com.elian.taskproject.data.model.User
+
+@Dao
+interface UserDAO
+{
+    @Query("SELECT * FROM user_table WHERE id = :id")
+    fun select(id: Long): User
+
+    @Insert
+    fun insert(user: User): Long
+}
