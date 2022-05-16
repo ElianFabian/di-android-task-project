5516a2caac79b011ee1b01789ee2781dd756ef55
Elián <elian.fabian.el.belaizi@gmail.com>
2022-05-16 12:20:31 +0200

    Added databaseWriteExecutor to AppDatabase
    Also deleted .allowMainThreadQueries()

--- app/src/main/java/com/elian/taskproject/data/database/AppDatabase.kt
+++ app/src/main/java/com/elian/taskproject/data/database/AppDatabase.kt
@@ -8,6 +8,8 @@ import com.elian.taskproject.data.database.dao.TaskDAO
 import com.elian.taskproject.data.database.dao.UserDAO
 import com.elian.taskproject.data.model.Task
 import com.elian.taskproject.data.model.User
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 @Database(version = 6, entities = [Task::class, User::class], exportSchema = false)
 abstract class AppDatabase : RoomDatabase()
@@ -20,6 +22,10 @@ abstract class AppDatabase : RoomDatabase()
         const val databaseName = "app_database"
         val instance get() = INSTANCE as AppDatabase
 
+        private const val NUMBER_OF_THREADS = 4;
+
+        val databaseWriteExecutor = Executors.newFixedThreadPool(NUMBER_OF_THREADS)
+
         @Volatile
         private var INSTANCE: AppDatabase? = null
 
@@ -30,7 +36,6 @@ abstract class AppDatabase : RoomDatabase()
                 return INSTANCE
                     ?: Room.databaseBuilder(context, AppDatabase::class.java, databaseName)
                         .fallbackToDestructiveMigration()
-                        .allowMainThreadQueries()
                         .build()
                         .also { INSTANCE = it }
             }
