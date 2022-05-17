cca0cc4cbb93593db7dd133bc08c25d2c878a5df
Elián <elian.fabian.el.belaizi@gmail.com>
2022-05-17 12:13:01 +0200

    Added Fragment.toast extension method

--- app/src/main/java/com/elian/taskproject/extensions/FragmentExtensions.kt
+++ app/src/main/java/com/elian/taskproject/extensions/FragmentExtensions.kt
@@ -1,6 +1,7 @@
 package com.elian.taskproject.extensions
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.annotation.IdRes
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
@@ -13,4 +14,9 @@ fun Fragment.navigate(@IdRes action: Int)
 fun Fragment.navigate(@IdRes action: Int, args: Bundle?)
 {
     NavHostFragment.findNavController(this).navigate(action, args)
+}
+
+fun Fragment.toast(text: CharSequence, duration: Int = Toast.LENGTH_SHORT)
+{
+    Toast.makeText(context, text, duration).show()
 }
