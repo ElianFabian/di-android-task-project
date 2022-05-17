adf76f03055570b733adfffa5c74a39829525b46
Elián <elian.fabian.el.belaizi@gmail.com>
2022-05-17 11:16:45 +0200

    Update TaskRoomRepository.kt

--- app/src/main/java/com/elian/taskproject/data/repository/TaskRoomRepository.kt
+++ app/src/main/java/com/elian/taskproject/data/repository/TaskRoomRepository.kt
@@ -2,21 +2,29 @@ package com.elian.taskproject.data.repository
 
 import com.elian.taskproject.data.database.AppDatabase
 import com.elian.taskproject.data.model.Task
-import com.elian.taskproject.ui.taskmanager.TaskManagerContract
 import com.elian.taskproject.ui.tasklist.TaskListContract
+import com.elian.taskproject.ui.taskmanager.TaskManagerContract
+import java.util.concurrent.Callable
+import java.util.concurrent.Future
 
 object TaskRoomRepository :
     TaskListContract.Repository,
     TaskManagerContract.Repository
 {
     private val taskDAO get() = AppDatabase.instance.taskDAO
-    private val executorService = AppDatabase.executorService
+
+    private val execute = AppDatabase.executorService::execute
+
+    private fun <T> submit(callable: Callable<T>): Future<T>
+    {
+        return AppDatabase.executorService.submit(callable)
+    }
 
     //region TaskListContract.Repository
 
     override fun getList(callback: TaskListContract.OnRepositoryCallback)
     {
-        val list = executorService.submit(taskDAO::selectAll).get()
+        val list = submit { taskDAO.selectAll() }.get()
 
         if (list.isEmpty())
         {
@@ -27,7 +35,7 @@ object TaskRoomRepository :
 
     override fun delete(callback: TaskListContract.OnRepositoryCallback, task: Task, position: Int)
     {
-        executorService.execute { taskDAO.delete(task) }
+        execute { taskDAO.delete(task) }
 
         callback.onDeleteSuccess(task, position)
     }
@@ -38,14 +46,14 @@ object TaskRoomRepository :
 
     override fun add(callback: TaskManagerContract.OnRepositoryCallback, task: Task)
     {
-        executorService.execute { taskDAO.insert(task) }
+        execute { taskDAO.insert(task) }
 
         callback.onAddSuccess()
     }
 
     override fun edit(callback: TaskManagerContract.OnRepositoryCallback, editedTask: Task, position: Int)
     {
-        executorService.execute { taskDAO.update(editedTask) }
+        execute { taskDAO.update(editedTask) }
 
         callback.onEditSuccess()
     }
